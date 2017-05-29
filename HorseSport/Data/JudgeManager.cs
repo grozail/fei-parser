9f4d4408f953698ce5123bfbe5ad3df82510dd1a
Ilya <grozail@yandex.ru>
2017-05-29 15:18:28 +0300

    first logging added

--- HorseSport/Data/JudgeManager.cs
+++ HorseSport/Data/JudgeManager.cs
@@ -3,11 +3,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NLog;
+using System.Xml.Linq;
 
 namespace HorseSport.Data {
-	class JudgeManager {
+	static class JudgeManager {
+		private static string FILE_PATH = "judges.xml";
 		private static Dictionary<string, Judge> mapFEIIDtoJudge;
+		private static Logger logger = LogManager.GetCurrentClassLogger();
 
+		internal static Dictionary<string, Judge> Data {
+			get {
+				return mapFEIIDtoJudge;
+			}
 
+			set {
+				mapFEIIDtoJudge = value;
+			}
+		}
+
+		public static void LoadData() {
+			Data = new Dictionary<string, Judge>();
+			try {
+				var document = XDocument.Load(FILE_PATH);
+				foreach (var el in document.Root.Elements()) {
+					try {
+						Data.Add(el.Attribute("FEIID").Value, 
+							new Judge(
+									el.Attribute("FEIID").Value, 
+									el.Attribute("NF").Value, 
+									el.Attribute("FirstName").Value, 
+									el.Attribute("FamilyName").Value, 
+									el.Attribute("OfficialStatus").Value, 
+									"-1"));
+					}
+					catch (Exception e) {
+						logger.Info(e, "EXCEPTION: occured in JudgeManager while reading data.|CAUSE: Nonunique or null FEIID");
+					}	
+				}
+			}
+			catch (Exception e) {
+				logger.Info(e, "EXCEPTION: occured in JudgeManager while loading data");
+			}
+		}
 	}
 }
--- HorseSport/Program.cs
+++ HorseSport/Program.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using HorseSport.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using NLog;
 
 namespace HorseSport {
 	static class Program {
+
+		private static Logger logger = LogManager.GetCurrentClassLogger();
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			JudgeManager.LoadData();
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainForm());
 		}
 	}
 }
