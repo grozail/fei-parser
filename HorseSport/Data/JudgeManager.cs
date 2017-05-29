4cf50a76bf90cb0b695cfb230dfb87fbf0ff77a9
Ilya <grozail@yandex.ru>
2017-05-29 13:43:09 +0300

    added judge manager

---
+++ HorseSport/Data/JudgeManager.cs
@@ -0,0 +1,13 @@
+﻿using HorseSport.Parser.Model.Living;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HorseSport.Data {
+	class JudgeManager {
+		private static Dictionary<string, Judge> mapFEIIDtoJudge;
+
+
+	}
+}
